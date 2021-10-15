8f17072b34890d94919c4b2a2aad5e34db47d989
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-10-15 15:40:28 -0300

    Insert categorie

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import pg from 'pg';
 
 const { Pool } = pg;
 const app = express();
+app.use(express.json());
 app.use(cors());
 
 const connection = new Pool({
@@ -13,3 +14,27 @@ const connection = new Pool({
     port: 5432,
     database: 'boardcamp'
 });
+
+app.post('/categories', async (req, res) => {
+    const name = req.body.name;
+    try {
+        if (name.length === 0) {
+            return res.sendStatus(400);
+        }
+
+        const duplicateCheck = await connection.query('SELECT * FROM categories WHERE name = $1', [name]);
+        if(duplicateCheck.rows.length !== 0) {
+            return res.sendStatus(409);
+        }
+
+        await connection.query('SELECT * FROM categories');
+        await connection.query('INSERT INTO categories (name) values ($1)', [name]);
+        res.sendStatus(201);
+        
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+})
+
+app.listen(4000);
