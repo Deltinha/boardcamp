389b48b22643c42312481b9f95b343d28f11a940
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-10-16 20:53:19 -0300

    Finished list and insert games

--- src/index.js
+++ src/index.js
@@ -33,4 +33,60 @@ app.post('/categories', async (req, res) => {
     }
 })
 
+app.get('/games', async (req, res) => {
+    let queryString ='%';
+    if (req.query.name !== undefined) {
+        queryString = req.query.name;
+    }
+    
+    try {   
+            const games = await connection.query(`
+                SELECT 
+                    games.*, 
+                    categories.name AS "categoryName" 
+                FROM games 
+                JOIN categories 
+                    ON categories.id = games."categoryId" 
+                WHERE LOWER(games.name) LIKE LOWER( $1 || '%')
+            `,[queryString]);
+            
+            res.send(games.rows).status(200);
+
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+})
+
+app.post('/games', async (req, res) => {
+    const {
+        name,
+        image,
+        stockTotal,
+        categoryId,
+        pricePerDay, 
+        } = req.body;
+
+    try {
+        const categorySelect = await connection.query('SELECT * FROM categories WHERE id = $1', [categoryId])
+
+        if (name.length === 0 || stockTotal <= 0 || pricePerDay <= 0 || categorySelect.rows.length === 0) {
+            return res.sendStatus(400);
+        }
+
+        const duplicateCheck = await connection.query('SELECT * FROM games WHERE name = $1', [name]);
+        if(duplicateCheck.rows.length !== 0) {
+            return res.sendStatus(409);
+        }
+
+        await connection.query('SELECT * FROM games');
+        await connection.query('INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay" ) values ($1, $2, $3, $4, $5)', [name, image, stockTotal, categoryId, pricePerDay]);
+        res.sendStatus(201);
+
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+})
+
 app.listen(4000);
